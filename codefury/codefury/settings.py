c42005adabc69a8f8db6eebb269153afbefaa22d
onigiriman7 <gamerholes@gmail.com>
2021-10-01 23:11:28 +0530

    heroku necessities

--- codefury/codefury/settings.py
+++ codefury/codefury/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#pt1&8wh(4)vk+)9l23k3vi_)wnz6#^&!36facy1n4u5cfnhbm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['codefury4-backend.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
